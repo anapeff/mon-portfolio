b48729f3d98dfd87ed08f0e652654d55253c2ce9
anapeff <anais.pefferkorn@outlook.fr>
2024-07-23 21:07:15 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
-import Error404 from './pages/Error/Error404';
+
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Error404 />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
     </Router>
