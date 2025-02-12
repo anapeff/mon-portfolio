bb22cee26d1914fdde8809009189498c654cbe1d
anapeff <anais.pefferkorn@outlook.fr>
2025-02-12 07:47:25 +0100

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,16 +15,12 @@ const About = () => {
             <span className="first-name">Anaïs</span>
             <span className="last-name">Pefferkorn</span>
           </h1>
-          <h2 className="title">Développeur web</h2>
+          <h2 className="title">Développeuse web</h2>
           <p className="description">Développeuse web spécialisée en front-end, j'ai été formée intensivement pendant 9 mois chez OpenClassrooms, où j'ai développé des compétences en React, Sass et Redux. Ces compétences me permettent de développer des applications web performantes et modernes.</p>
-          <p className="description">Avant cela, j'ai travaillé plusieurs années en tant qu'agente de support pour un logiciel d'huissier, ce qui m'a dotée de compétences essentielles telles que l’organisation, l’écoute, le conseil ainsi que des qualités relationnelles importantes. Toujours motivée par l'apprentissage de nouvelles technologies, je suis enthousiaste à l'idée de participer à de nouveaux projets.</p>
-
+          
           <div className="buttons">
             <a href="https://github.com/anapeff" target="_blank" rel="noopener noreferrer">
               GITHUB
-            </a>
-            <a href="https://linkedin.com/in/anaïs-p-561140263" target="_blank" rel="noopener noreferrer">
-              LINKEDIN
             </a>
           </div>
         </div>
